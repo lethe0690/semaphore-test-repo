db668b93503b53e375e1a53bb1c8ce4ca4d97074
peiwen <ekaidakimas@gmail.com>
2021-03-17 14:05:05 -0400

    feat: [O-123] add a divide

--- src/index.ts
+++ src/index.ts
@@ -11,3 +11,7 @@ export function minus (a: number, b: number) {
 export function mul (a: number, b: number) {
     return a * b;
 }
+
+export function divide (a: number, b: number) {
+    return a / b;
+}
