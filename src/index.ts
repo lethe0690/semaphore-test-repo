aa5fbe98548ad03c95a102eeb4a8952eeb9bc6df
peiwen <ekaidakimas@gmail.com>
2021-03-17 11:53:26 -0400

    feat(O-359) title
    feat(O-359) content

--- src/index.ts
+++ src/index.ts
@@ -7,3 +7,7 @@ export function add (a: number, b: number) {
 export function minus (a: number, b: number) {
     return a - b;
 }
+
+export function mul (a: number, b: number) {
+    return a * b;
+}
